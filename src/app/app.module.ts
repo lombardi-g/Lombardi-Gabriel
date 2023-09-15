74d34174a056d5519dad2e99e19011ec1f573da9
lombardi-g <gabriellomba@gmail.com>
2023-09-15 12:55:14 -0300

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CardComponent } from './card/card.component';
+import { TitleComponent } from './title/title.component';
+import { LogoComponent } from './logo/logo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CardComponent,
+    TitleComponent,
+    LogoComponent
   ],
   imports: [
     BrowserModule,
